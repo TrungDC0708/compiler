87c9565d17a240cdd9190e4c82db4ccd75d928ad
nqvinh00 <nqvinh00@gmail.com>
2022-03-27 14:48:06 +0700

    update

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+import sys
 from buffer import Buffer
 from scanner import LexicalScanner
 
 
 if __name__ == '__main__':
+    args = sys.argv
+    if len(args) < 1:
+        raise RuntimeError('expected 1 file path argument')
+
     b = Buffer()
     scanner = LexicalScanner()
     rule = scanner.tokenize()
 
     token, lexeme = [], []
-    for value in b.load_file("hello.vc"):
+    for value in b.load_file(args[1]):
         t, l, r, c = scanner.analyze(value, rule)
         token += t
         lexeme += l
