55d22bd59a0e555cc2a8b762b2d8fc5f661e77b8
nqvinh00 <32157740+nqvinh00@users.noreply.github.com>
2022-04-25 20:34:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,17 @@
 import sys
-from buffer import Buffer
+from file import load_code_file, write_to_file
 from scanner import LexicalScanner
+from rules import Rule
 
 if __name__ == '__main__':
-    # args = sys.argv
-    # if len(args) < 1:
-    #     raise RuntimeError('expected 1 file path argument')
+    args = sys.argv
+    if len(args) < 1:
+        raise RuntimeError('expected 1 file path argument')
 
-    scanner = LexicalScanner()
-    with open('hello.vc') as f:
-        lines = f.read()
-    list=[]
-    for i in lines:
-        list.append(i)
-    print(list)
+    rule = Rule('rules.csv')
+    rule.load_rule()
+    scanner = LexicalScanner(rule)
+    list = load_code_file(args[1])
     token = scanner.tokenize(list)
-    # token, lexeme = [], []
-    # for value in b.load_file(args[1]):
-    #     t, l, r, c = scanner.analyze(value, rule)
-    #     token += t
-    #     lexeme += l
-    #
-    #
-    # print(token)
-    # f = open('output.vctok', 'w')
-    # for l in set(lexeme):
-    #     f.write(l+"\n")
-    # f.close()
+    write_to_file(token)
+
